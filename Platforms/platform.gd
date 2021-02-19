a755a66360f51df5d6fca6c86c9a4d497ea6b10e
kyuyoungkim <47513498+kyuyoungkim@users.noreply.github.com>
2021-02-19 18:19:21 -0500

    Moving Hazards & Platform

---
+++ Platforms/platform.gd
@@ -0,0 +1,39 @@
+extends CollisionShape2D
+
+export var speed : int = 100
+export var moveDist : int = 100
+
+onready var startX : float = position.x
+onready var targetX : float = position.x + moveDist
+
+func _physics_process(delta):
+	
+	# move to the "targetX" position
+	position.x = move_to(position.x, targetX, speed * delta)
+	
+	# if we're at our target, move in the other direction
+	if position.x == targetX:
+		if targetX == startX:
+			targetX = position.x + moveDist
+		else:
+			targetX = startX
+			
+# moves "current" towards "to" in an increment of "step"
+func move_to (current, to, step):
+	
+	var new = current
+	
+	# are we moving positive?
+	if new < to:
+		new += step
+		
+		if new > to:
+			new = to
+	#are we moving negative?
+	else:
+		new -= step
+		
+		if new < to:
+			new = to
+			
+	return new
---
+++ alligator.gd
@@ -0,0 +1,39 @@
+extends CollisionShape2D
+
+export var speed : int = 100
+export var moveDist : int = 100
+
+onready var startY : float = position.y
+onready var targetY : float = position.y + moveDist
+
+func _physics_process(delta):
+	
+	# move to the "targetY" position
+	position.y = move_to(position.y, targetY, speed * delta)
+	
+	# if we're at our target, move in the other direction
+	if position.y == targetY:
+		if targetY == startY:
+			targetY = position.y + moveDist
+		else:
+			targetY = startY
+			
+# moves "current" towards "to" in an increment of "step"
+func move_to (current, to, step):
+	
+	var new = current
+	
+	# are we moving positive?
+	if new < to:
+		new += step
+		
+		if new > to:
+			new = to
+	#are we moving negative?
+	else:
+		new -= step
+		
+		if new < to:
+			new = to
+			
+	return new
